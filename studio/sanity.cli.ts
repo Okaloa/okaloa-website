ef73dd8221ea8f344f9046f26cb1c4becef5878d
Iniazst <iniaz.steyaert@gmail.com>
2026-07-28 10:07:16 +0200

    chore: configure studioHost in sanity.cli.ts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -2,8 +2,8 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    // Replace this with your actual Sanity Project ID from sanity.io/manage
     projectId: 'b7wqv3yo',
     dataset: 'production'
-  }
+  },
+  studioHost: 'okaloa'
 })
